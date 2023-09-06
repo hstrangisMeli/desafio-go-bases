35aede6d999499b922fbf9358f545b5cddddc5f9
hstrangis <nicolas.strangis@mercadolibre.com>
2023-09-06 08:38:16 -0300

    Primer versión, falta mejorar errores, agregar test y perfeccionar

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,13 +1,94 @@
 package tickets
 
+import (
+	"errors"
+	"strconv"
+)
+
 type Ticket struct {
+	Id          string
+	Name        string
+	Email       string
+	Destination string
+	Time        string
+	Tickets     []Ticket
+}
+
+const (
+	_pos_id          = 0
+	_pos_name        = 1
+	_pos_email       = 2
+	_pos_destination = 3
+	_pos_time        = 4
+)
+
+const (
+	_early = iota
+	_morning
+	_late
+	_night
+)
+
+func GenerateTicket(datos []string) Ticket {
+	t := Ticket{}
+	t.Id = datos[_pos_id]
+	t.Name = datos[_pos_name]
+	t.Email = datos[_pos_email]
+	t.Destination = datos[_pos_destination]
+	t.Time = datos[_pos_time]
+	return t
 }
 
 // ejemplo 1
-func GetTotalTickets(destination string) (int error) {}
+func GetTotalTickets(arrTickets *[]Ticket, destination string) int {
+	count := 0
+	for _, t := range *arrTickets {
+		if t.Destination == destination {
+			count++
+		}
+	}
+	return count
+}
 
 // ejemplo 2
-func GetMornings(time string) (int error) {}
+func GetPeriod(hour int) int {
+	switch {
+	case hour >= 0 && hour < 6:
+		return _early
+	case hour >= 6 && hour < 12:
+		return _morning
+	case hour >= 12 && hour < 18:
+		return _late
+	default:
+		return _night
+	}
+}
+
+func GetTicketsByPeriod(arrTickets *[]Ticket, time string) (int, error) {
+	var period, count int
+	hour, err := strconv.Atoi(time[:2])
+	count = 0
+	period = GetPeriod(int(hour))
+	for _, t := range *arrTickets {
+		var ticketHour int
+		ticketHour, err = strconv.Atoi(t.Time[:2])
+		if period == GetPeriod(ticketHour) {
+			count++
+		}
+	}
+	return count, err
+}
 
 // ejemplo 3
-func AverageDestination(destination string, total int) (int error) {}
+func AverageDestination(arrTickets *[]Ticket, destination string, total int) (float64, error) {
+	count := 0
+	if total == 0 {
+		return 0, errors.New("No hay tickets")
+	}
+	for _, t := range *arrTickets {
+		if destination == t.Destination {
+			count++
+		}
+	}
+	return float64(count) / float64(total) * 100, nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
+	"os"
 )
 
+func FileParserToTickets(f *os.File, arrTickets *[]tickets.Ticket) {
+	csvReader := csv.NewReader(f)
+	for {
+		rec, err := csvReader.Read()
+		if err != nil && err.Error() == "EOF" {
+			break
+		}
+		t := tickets.GenerateTicket(rec)
+		*arrTickets = append(*arrTickets, t)
+	}
+}
+
 func main() {
-	total, err := tickets.GetTotalTickets("Brazil")
+	arrTickets := []tickets.Ticket{}
+	f, err := os.Open("/Users/nstrangis/GoBoot/desafio-go-bases/tickets.csv")
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	defer f.Close()
+
+	FileParserToTickets(f, &arrTickets)
+
+	//REQ1
+	total := tickets.GetTotalTickets(&arrTickets, "Brazil")
+	fmt.Println(total)
+
+	//REQ2
+	totalHora, err := tickets.GetTicketsByPeriod(&arrTickets, "05")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(totalHora)
+	}
+
+	//REQ3
+	average, err := tickets.AverageDestination(&arrTickets, "Argentina", len(arrTickets))
+	fmt.Println(average)
 }
